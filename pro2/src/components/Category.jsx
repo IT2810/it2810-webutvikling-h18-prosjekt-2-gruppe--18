5f48d9797c21edc5d9eb3c622849183af1e220ff
simnoba <simnoba@gmail.com>
2018-09-18 18:57:41 +0200

    Addet en sidenav, funker ikke helt 100% enda. Mangler bare å få knappen som åpner opp sideNav til å fungere igjen, sikkert noe jeg overså i CSS implementasjonen

--- pro2/src/components/Category.jsx
+++ pro2/src/components/Category.jsx
@@ -5,7 +5,7 @@ class Category extends Component {
   state = {
     textType: null,
     imageType: null,
-    soundType: null
+    soundType: null,
   };
 
   sendInfoUp() {
@@ -21,27 +21,63 @@ class Category extends Component {
   handleChangeValueText = e => {
     this.props.onChangeValue("Text: " + e);
   };
+
+  //Closes the navigation panel
+  closeNav = e => {
+        e.preventDefault();
+        document.getElementById("mySidenav").style.width = "0";
+    };
+//Her er løsningen basicly å dele opp i en div, med 2 sub divs som er like, men omringet av en mobile/nonMobile div
+    //Slik at man kan endre enkelt hvem som vises med CSS
   render() {
-    return (
-      <div id="Category-container">
-        <Choices
-          type="image"
-          value={this.state.imageType}
-          onChangeValue={this.handleChangeValueImage}
-        />
-        <Choices
-          type="sound"
-          value={this.state.soundType}
-          onChangeValue={this.handleChangeValueSound}
-        />
-        <Choices
-          type="text"
-          value={this.state.textType}
-          onChangeValue={this.handleChangeValueText}
-        />
-      </div>
-    );
-  }
+          return(
+             <div>
+                 <div id="nonMobileDiv">
+              <div id="Category-container">
+                  <Choices
+                      type="image"
+                      value={this.state.imageType}
+                      onChangeValue={this.handleChangeValueImage}
+                  />
+                  <Choices
+                      type="sound"
+                      value={this.state.soundType}
+                      onChangeValue={this.handleChangeValueSound}
+                  />
+                  <Choices
+                      type="text"
+                      value={this.state.textType}
+                      onChangeValue={this.handleChangeValueText}
+                  />
+              </div>
+             </div>
+
+              <div id="mySidenav" className="sidenav">
+                  /* The button for closing the sidenav*/
+                  <a href={null} className="closebtn" onClick={this.closeNav}>&times;</a>
+              <div id="Category-container">
+                  <Choices
+                      type="image"
+                      value={this.state.imageType}
+                      onChangeValue={this.handleChangeValueImage}
+                  />
+                  <Choices
+                      type="sound"
+                      value={this.state.soundType}
+                      onChangeValue={this.handleChangeValueSound}
+                  />
+                  <Choices
+                      type="text"
+                      value={this.state.textType}
+                      onChangeValue={this.handleChangeValueText}
+                  />
+              </div>
+              </div>
+             </div>
+
+
+          )
+      }
 }
 
 export default Category;
