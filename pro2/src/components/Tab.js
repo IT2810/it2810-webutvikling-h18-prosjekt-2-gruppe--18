d035d08fd92517e3da76a2754577c0b1a11f782e
filiphag <filiphag@stud.ntnu.no>
2018-09-20 09:37:13 +0200

    fikset buggen der bilde forandret seg on update

--- pro2/src/components/Tab.js
+++ pro2/src/components/Tab.js
@@ -7,128 +7,122 @@ import Visuals from "./Visuals";
  * Tab component that takes care of text, visuals and audio.
  */
 class Tab extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            audio: {
-                name: null,
-                file: null
-            },
-            visual: "",
-            text: {
-                category: "",
-                selectionNumber: null
-            }
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: {
+        name: null,
+        file: null
+      },
+      visual: "",
+      text: {
+        category: "",
+        selectionNumber: null
+      }
+    };
 
-        this.oldSoundType = {};
-        this.oldstatus = {};
-        this.oldImageType = {};
-        this._getAudioFile = this._getAudioFile.bind(this);
+    this.oldSoundType = {};
+    this.oldstatus = {};
+    this.oldImageType = {};
+    this._getAudioFile = this._getAudioFile.bind(this);
+  }
+
+  render() {
+    // sound
+    if (this.oldSoundType !== this.props.soundType) {
+      let unique = Math.round(Math.random() * 4 + 0.5);
+      let sound_name = this.props.soundType + unique.toString();
+      this._getAudioFile(sound_name);
+    }
+    this.oldSoundType = this.props.soundType;
+
+    // image
+    if (this.oldImageType !== this.props.typer.imageType) {
+      let unique = Math.round(Math.random() * 4 + 0.5);
+      this.state.visual = this.props.typer.imageType + unique.toString();
+      this.state.visual = this.state.visual.toLocaleLowerCase();
+      console.log("forandret: ", this.state.visual);
     }
 
-    render() {
+    this.oldImageType = this.props.typer.imageType;
 
-
-        // sound
-        if (this.oldSoundType !== this.props.soundType) {
-            let unique = Math.round(Math.random() * 4 + 0.5);
-            let sound_name = this.props.soundType + unique.toString();
-            this._getAudioFile(sound_name);
-        }
-        this.oldSoundType = this.props.soundType;
-
-
-        // image
-        if (this.oldImageType !== this.props.typer.imageType) {
-            let unique = Math.round(Math.random() * 4 + 0.5);
-            this.state.visual = this.props.typer.imageType + unique.toString();
-            this.state.visual = this.state.visual.toLocaleLowerCase();
-            console.log("forandret: ", this.state.visual);
-        }
-
-        this.oldImageType = this.props.typer.imageType;
-
-
-        // text
-        if(this.oldstatus.textType !== this.props.typer.textType) {
-            let unique = Math.round(Math.random() * 4 + 0.5);
-            this.state.text.selectionNumber = unique;
-            this.state.text.category = this.props.typer.textType;
-            console.log("forandret: ", this.state.text.category);
-        }
-
-        this.oldstatus.textType = this.props.typer.textType;
-
-        return (
-            <div className={this.props.activeStatus}>
-                <h1>{this.props.name}</h1>
-                <AudioComponent
-                    audio={this.state.audio}
-                    getAudioFile={this._getAudioFile}
-                />
-                <TextComponent
-                    text={this.state.text}/>
-                <Visuals bilde={this.state.visual} />
-            </div>
-        );
+    // text
+    if (this.oldstatus.textType !== this.props.typer.textType) {
+      let unique = Math.round(Math.random() * 4 + 0.5);
+      this.state.text.selectionNumber = unique;
+      this.state.text.category = this.props.typer.textType;
+      console.log("forandret: ", this.state.text.category);
     }
 
-    /**
-     * Fetches the sound and stores it in the state.
-     * @private
-     */
-    _getAudioFile(sound_name) {
-        let name = sound_name;
+    this.oldstatus.textType = this.props.typer.textType;
 
-        // validation 1
-        // if name is null no option has been selected.
-        if (name === null) {
-            return;
-        }
+    return (
+      <div className={this.props.activeStatus}>
+        <h1>{this.props.name}</h1>
+        <AudioComponent
+          audio={this.state.audio}
+          getAudioFile={this._getAudioFile}
+        />
+        <TextComponent text={this.state.text} />
+        <Visuals bilde={this.state.visual} tabInfo={this.props.typer} />
+      </div>
+    );
+  }
 
-        // validation 2
-        // mostly just used once.
-        if (!name.endsWith(".mp3")) {
-            name = name + ".mp3";
-        }
+  /**
+   * Fetches the sound and stores it in the state.
+   * @private
+   */
+  _getAudioFile(sound_name) {
+    let name = sound_name;
 
-        let file_name = name.toLowerCase();
-        console.log(this.props.id ,"filename: ", file_name);
-        let file_path = "./media/audio/" + file_name;
-
-
-        // TODO check if necessary
-        let myInit = {
-            method: "GET",
-            headers: new Headers(),
-            mode: "cors",
-            cache: "default"
-        };
-
-        // fetches the sound file and updates the
-        // state so that the AudioComponent gets updated.
-        fetch(file_path, myInit)
-            .then(response => {
-                return response.blob();
-            })
-            .then(blob => {
-                let obj = URL.createObjectURL(blob);
-
-                this.setState({
-                    audio: {
-                        name: file_name,
-                        file: obj
-                    }
-                });
-
-                console.log("Changed state audio to: " + this.state.audio.file);
-                return true;
-            })
-            .catch(error => {
-                console.log(error);
-                return false;
-            });
+    // validation 1
+    // if name is null no option has been selected.
+    if (name === null) {
+      return;
     }
+
+    // validation 2
+    // mostly just used once.
+    if (!name.endsWith(".mp3")) {
+      name = name + ".mp3";
+    }
+
+    let file_name = name.toLowerCase();
+    console.log(this.props.id, "filename: ", file_name);
+    let file_path = "./media/audio/" + file_name;
+
+    // TODO check if necessary
+    let myInit = {
+      method: "GET",
+      headers: new Headers(),
+      mode: "cors",
+      cache: "default"
+    };
+
+    // fetches the sound file and updates the
+    // state so that the AudioComponent gets updated.
+    fetch(file_path, myInit)
+      .then(response => {
+        return response.blob();
+      })
+      .then(blob => {
+        let obj = URL.createObjectURL(blob);
+
+        this.setState({
+          audio: {
+            name: file_name,
+            file: obj
+          }
+        });
+
+        console.log("Changed state audio to: " + this.state.audio.file);
+        return true;
+      })
+      .catch(error => {
+        console.log(error);
+        return false;
+      });
+  }
 }
 export default Tab;
